0a987fdeb945fb3daec504047e27de353ffc518f
Zakaria  Hannane <zakariahannane28@gmail.com>
2025-11-30 17:54:38 +0100

    Refactor BankAccount class methods
    Refactor BankAccount class for improved readability.

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,18 +1,1 @@
-class BankAccount:
-    def __init__(self, initial_balance=0):
-        self.__account_balance = initial_balance  
-
-    def deposit(self, amount):
-        if amount > 0:
-            self.__account_balance += amount
-
-    def withdraw(self, amount):
-        if amount > 0 and amount <= self.__account_balance:
-            self.__account_balance -= amount
-            return True
-        return False
-
-  def display_balance(self):
-    print(f"Current Balance: ${self.__account_balance:.2f}")
-
-
+class BankAccount: def __init__(self, initial_balance=0): self.__account_balance = initial_balance def deposit(self, amount): if amount > 0: self.__account_balance += amount def withdraw(self, amount): if amount > 0 and amount <= self.__account_balance: self.__account_balance -= amount return True return False def display_balance(self): print(f"Current Balance: ${self.__account_balance}")
